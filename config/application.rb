8edd8dec6f1fae7a89a061a93b53c8dd62c90070
Rinat <rinat@gabidullin.com>
2014-11-17 18:53:26 +0400

    make it better

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-module centrsogl
+module Kbk
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -20,6 +20,6 @@ module centrsogl
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
-    config.assets.precompile = false
+    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
   end
 end
