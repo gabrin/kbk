36ccfcb22bf421cb447e2f43ce2effbeefe2c1c1
Rinat <rinat@gabidullin.com>
2014-11-17 18:27:41 +0400

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Kbk::Application.configure do
+centrsogl::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
