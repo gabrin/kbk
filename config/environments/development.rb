adeb499bf3721a5526cc8d5f34d31442e48c27b8
Rinat <rinat@gabidullin.com>
2014-11-17 18:33:01 +0400

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Kbk::Application.configure do
+centrsogl::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
