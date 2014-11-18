e980a91c70b4b2aa2f9275e3cc8fa30451c142a6
Rinat <rinat@gabidullin.com>
2014-11-18 10:12:41 +0400

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Centrsogl::Application.configure do
 
   config.action_mailer.default_url_options = { host: Settings.host }
 
-  mailer_config = Settings.smtp_settings.to_hash
+  #mailer_config = Settings.smtp_settings.to_hash
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = mailer_config
